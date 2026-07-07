dc43610783cab137c0ab54b4657ff0bdcdd6f973
H-ricky-w <hricky561@gmail.com>
2026-07-07 12:10:27 -0300

    sdfg

--- script/comandos.js
+++ script/comandos.js
@@ -26,6 +26,17 @@ const botoes = {
     25:false,
 }
 
+function colorirBotoes()
+{
+    for(let i=1; i<=25; i++)
+    {
+        if(!botoes[i])
+        {
+            document.getElementById('botao'+i).style.color = "rgb(99, 199, 182)"
+        }
+    }
+}
+
 function enviarResposta()
 {
     let resposta = 0
@@ -36,24 +47,58 @@ function enviarResposta()
             resposta++
         }
     }
+
+    responderPergunta(resposta)
+
+    document.getElementById('resposta').textContent = resposta
+    for(let i=1;i<=25;i++)
+    {
+        botoes[i]=false
+        document.getElementById('botao'+i).style.backgroundColor = "rgb(99, 199, 182)"
+    }
 }
 
 function alternarBotao(botao)
 {
-    botoes[botao] = true;
-    document.getElementById('botao'+botao).style.backgroundColor = "rgb(73, 211, 31)";
+    if(!botoes[botao])
+    {
+        botoes[botao] = true;
+        document.getElementById('botao'+botao).style.backgroundColor = "rgb(73, 211, 31)";
+    }
+    else
+    {
+        botoes[botao] = false;
+        document.getElementById('botao'+botao).style.backgroundColor = 'rgb(99, 199, 182)'
+    }
 }
 
-function colorirBotoes()
+function gerarPergunta() // gera uma Soma
 {
-    for(let i=1; i<=25; i++)
-    {
-        if(botoes[i])
-        {
-            document.getElementById('botao'+i).style.color = "rgb(99, 199, 182)"
-        }
-    }
+    let resultado;
+    let parcela1;
+    let parcela2;
+
+    do{
+        resultado = Math.floor(Math.random() * 25);
+        parcela1 = Math.floor(Math.random() * 24);
+        parcela2 = resultado - parcela1;
+    }while(parcela2<0);
+
+    const container = document.getElementById("pergunta");
+    let pergunta = "<h3>selecione "+parcela1+"+"+parcela2+" quadrados</h3>";
+    container.innerHTML = pergunta;
+}
+
+function responderPergunta(resposta)
+{
+    
 }
 
 function carregarComponente(idElemento, arquivoHTML)
 {}
+
+document.addEventListener("DOMContentLoaded", () => {
+    carregarComponente("espaco-cabecalho", "cabecalho.html");
+    carregarComponente("espaco-rodape", "rodape.html");
+    gerarPergunta()
+});
